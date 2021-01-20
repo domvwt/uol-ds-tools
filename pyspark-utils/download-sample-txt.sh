4d6f38e2b315a9646ed0f89063fbd4496c67c378
Dominic Thorn <dominic.thorn@gmail.com>
2021-01-20 09:43:56 +0000

    Add pyspark utils

---
+++ pyspark-utils/download-sample-txt.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/bash
+wget https://www.gutenberg.org/files/84/84-0.txt -O frankenstein.txt
---
+++ pyspark-utils/wordcounts.py
@@ -0,0 +1,36 @@
+#! /opt/spark/bin/pyspark
+import re
+from pathlib import Path
+
+
+INPUT_TXT = "frankenstein.txt"
+
+
+myfile = Path(INPUT_TXT).absolute()
+rdd_txt = sc.textFile(f"file:///{myfile}")
+
+# Simple word counts splitting on whitespace
+counts = (
+    rdd_txt.flatMap(lambda line: line.split())
+    .map(lambda word: (word, 1))
+    .reduceByKey(lambda a, b: a + b)
+    .map(lambda a: (a[1], a[0]))
+)
+
+res1 = counts.collect()[:20]
+for i in res1:
+    print(i)
+print()
+
+# Word counts splitting on non word elements
+word_counts = (
+    rdd_txt.flatMap(lambda line: re.split(r"\W+", line))
+    .map(lambda word: (word, 1))
+    .reduceByKey(lambda a, b: a + b)
+    .map(lambda a: (a[1], a[0]))
+)
+
+res2 = word_counts.collect()[:20]
+for i in res2:
+    print(i)
+print()
