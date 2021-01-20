5b9a9ed94cbd065074510b28e2a45ae5c322c5a9
Dominic Thorn <dominic.thorn@gmail.com>
2021-01-20 22:34:51 +0000

    Fix package name for backward compat

--- pyspark-utils/install-script.sh
+++ pyspark-utils/install-script.sh
@@ -11,7 +11,7 @@ sudo apt-get install openjdk-8-jre -y && \
 wget https://apache.mirrors.nublue.co.uk/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz && \
 tar -xzf spark-3.0.1-bin-hadoop3.2.tgz && \
 sudo mv spark-3.0.1-bin-hadoop3.2 /opt/spark && \
-sudo apt-get install ipython3 pip -y && \
+sudo apt-get install ipython3 python3-pip -y && \
 pip install pyspark==3.0 && \
 echo 'export SPARK_HOME=/opt/spark' >> ~/.bash_profile && \
 echo 'export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin' >> ~/.bash_profile && \
