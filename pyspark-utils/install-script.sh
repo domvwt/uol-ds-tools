9f2b39339c029e04156f41e706dcaa1c6d0e8cc2
Dominic Thorn <dominic.thorn@gmail.com>
2021-01-20 12:24:22 +0000

    Prevent var evaluation in bash_profile

--- pyspark-utils/install-script.sh
+++ pyspark-utils/install-script.sh
@@ -13,9 +13,9 @@ tar -xzf spark-3.0.1-bin-hadoop3.2.tgz && \
 sudo mv spark-3.0.1-bin-hadoop3.2 /opt/spark && \
 sudo apt-get install ipython3 pip -y && \
 pip install pyspark==3.0 && \
-echo "export SPARK_HOME=/opt/spark" >> ~/.bash_profile && \
-echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bash_profile && \
-echo "export PYSPARK_DRIVER_PYTHON=/usr/bin/ipython3" >> ~/.bash_profile && \
-echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.bash_profile && \
+echo 'export SPARK_HOME=/opt/spark' >> ~/.bash_profile && \
+echo 'export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin' >> ~/.bash_profile && \
+echo 'export PYSPARK_DRIVER_PYTHON=/usr/bin/ipython3' >> ~/.bash_profile && \
+echo 'export PYSPARK_PYTHON=/usr/bin/python3' >> ~/.bash_profile && \
 source ~/.bash_profile && \
 echo "Type 'pyspark' on the command line to start session"
