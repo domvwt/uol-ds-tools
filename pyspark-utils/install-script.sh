6694c08502e3cb239b9b7a9a5c8b9535b8c3117b
Dominic Thorn <dominic.thorn@gmail.com>
2021-01-20 11:39:50 +0000

    Source bash_profile after it's been updated

--- pyspark-utils/install-script.sh
+++ pyspark-utils/install-script.sh
@@ -11,11 +11,11 @@ sudo apt-get install openjdk-8-jre -y && \
 wget https://apache.mirrors.nublue.co.uk/spark/spark-3.0.1/spark-3.0.1-bin-hadoop3.2.tgz && \
 tar -xzf spark-3.0.1-bin-hadoop3.2.tgz && \
 sudo mv spark-3.0.1-bin-hadoop3.2 /opt/spark && \
-source ~/.bash_profile && \
 sudo apt-get install ipython3 pip -y && \
 pip install pyspark==3.0 && \
 echo "export SPARK_HOME=/opt/spark" >> ~/.bash_profile && \
 echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bash_profile && \
 echo "export PYSPARK_DRIVER_PYTHON=/usr/bin/ipython3" >> ~/.bash_profile && \
 echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.bash_profile && \
+source ~/.bash_profile && \
 echo "Type 'pyspark' on the command line to start session"
